aaaf1f057b37b7752f074496354f4f013aede5e1
HIMANSHU SHARMA <himanshusharma@hs.local>
2026-09-03 02:06:54 +0530

    Update project

--- backend/ejs/index.js
+++ backend/ejs/index.js
@@ -1,15 +1,39 @@
-// const express = require('express');
-// const app = express();
+const express = require('express');
+const app = express();
+const path = require('path');
 
-// const port=8080;
+const port=8080;
 
-// app.set("view engine","ejs");
+// const publicPath = path.join(__dirname, "public");
+app.use(express.static(path.join(__dirname, "public/css")));
+app.use(express.static(path.join(__dirname, "public/js")));
 
-// app.get("/",(req, res)=>{
-//     res.render("home.ejs");
-// })
+app.set("view engine","ejs");
+app.set("views", path.join(__dirname, "views"));
 
-// app.listen(port,()=>{
-//     console.log(`Server is running on port ${port}`);
+app.get("/",(req, res)=>{
+    res.render("home");
+})
+
+app.get("/rolldice",(req, res)=>{
+    let randomNumber = Math.floor(Math.random() * 6) + 1;
+    res.render("rolldice",{randomNumber});
+});
+
+// app.get("/ig/:username",(req, res)=>{
+//     let {username} = req.params;
+//     res.render("instagram", {username });
 // });
+
+app.get("/ig/:username",(req, res)=>{
+    let {username} = req.params;
+    const instadata=require("./data.json");
+    const data=instadata[username];
+    console.log(data);
+    res.render("instagram.ejs", {data});
+});
+
+app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+});
 
